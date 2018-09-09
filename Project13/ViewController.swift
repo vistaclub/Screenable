50ff74fbcbd996665a41e61aec4221b5c5766000
Jason Wong <jawong71@gmail.com>
2018-09-09 08:36:41 -0700

    Initial Commit

---
+++ Project13/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Jason Wong on 2018-09-09.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
