02b35c6e55cfc1a4c8225252a8ca265c5b3d8327
Jason Wong <jawong71@gmail.com>
2018-09-29 18:56:28 -0700

    first commit

---
+++ Project13/Screenshot.swift
@@ -0,0 +1,13 @@
+//
+//  Screenshot.swift
+//  Project13
+//
+//  Created by Jason Wong on 2018-09-09.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+class Screenshot: NSObject {
+
+}
